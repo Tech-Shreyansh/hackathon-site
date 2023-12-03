d072f80d3804f8f78100de244c93aeed5dc2c0b6
Tech-Shreyansh <shreyanshagarwal.16.2022@gmail.com>
2023-12-03 17:00:06 +0530

    added problem statement section

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProblemStatement from "./ProblemStatement";
+
+const HomePage = () => {
+    return <div>
+       <ProblemStatement />
+    </div>
+}
+
+export default HomePage;
