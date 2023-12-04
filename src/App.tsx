7769dab6ba2a9ed6245f013957867647dd9ff56e
Tech-Shreyansh <shreyanshagarwal.16.2022@gmail.com>
2023-12-04 19:50:34 +0530

    merged form and done linking

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Form from './components/Register/Form';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/register" element={<Form />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/HomePage/Intro.tsx
+++ src/components/HomePage/Intro.tsx
@@ -1,12 +1,16 @@
 import sideImg from '../../assets/sideImg.jpg'
+import { useNavigate } from "react-router-dom";
 
 function Intro() {
+
+    const navHandler  = useNavigate()
+
     return (
         <div id='intro'>
             <div className="px-20 py-12 flex flex-col md:flex-row justify-between border-b-4 border-black items-center">
                 <div className='text-left text-lg font-medium md:text-xl pr-12 lg:text-2xl'>
                     Hackubator, a two-day event organized by SDC-SI at AKGEC,Ghaziabad, brings together 2nd and 3rd-year college students to collaborate on building projects and solutions addressing common issues within their college community.
-                    <button className="my-12 text-white flex cursor-pointer items-center rounded-md border-2 border-black bg-violet-400 px-10 py-3 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none">Register</button>
+                    <button onClick={()=>navHandler("/register")} className="my-12 text-white flex cursor-pointer items-center rounded-md border-2 border-black bg-violet-400 px-10 py-3 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none">Register</button>
                 </div>
                 <div className='flex align-middle justify-center -mt-6'><img src={sideImg} className='w-3/4'></img></div>
             </div>
